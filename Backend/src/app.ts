538c9fd343128f0dbd04f9605ef44a063d5d1d20
adrian enis <adrianenis2000@gmail.com>
2026-05-19 19:57:55 -0400

    feat: add router & errorHandler

--- Backend/src/app.ts
+++ Backend/src/app.ts
@@ -1,19 +1,21 @@
 import express, { type Express } from 'express'
 import cors from 'cors'
 import dotenv from 'dotenv'
-
+import router from './06-routes/index.js'
+import { errorHandler } from './07-middlerware/errorHandler.js'
 dotenv.config()
 
 const app: Express = express()
 const PORT = process.env.PORT || 3000
 
-app.use(cors())
-app.use(express.json())
+app.use(cors()) // Middleware para habilitar CORS, permitiendo que el frontend pueda hacer solicitudes al backend desde un dominio diferente
+app.use(express.json()) // Middleware para parsear el cuerpo de las solicitudes como JSON
+app.use('/api', router) 
 
-app.get('/api/health', (_req, res) => {
+app.get('/api/health', (_req, res) => { // Ruta de prueba para verificar que el servidor está funcionando correctamente
   res.json({ status: 'ok' })
 })
-
+app.use(errorHandler)
 app.listen(PORT, () => {
   console.log(`Servidor corriendo en http://localhost:${PORT}`)
 })
